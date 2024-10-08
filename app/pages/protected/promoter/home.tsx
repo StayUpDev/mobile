b545071648cd15d08ab901ac12937806e7b84e52
rickturner2001 <stayup.dev@gmail.com>
2024-10-09 00:44:21 +0200

    spostato docker compose su API

--- app/pages/protected/promoter/home.tsx
+++ app/pages/protected/promoter/home.tsx
@@ -1,5 +1,21 @@
+import { Box } from "@/components/ui/box";
+import { Fab, FabIcon, FabLabel } from "@/components/ui/fab";
+import { AddIcon } from "@/components/ui/icon";
 import { Text } from "@/components/ui/text";
 
 export default function Page() {
-  return <Text>Promoter</Text>;
+  return (
+    <Box>
+      <Fab
+        size="md"
+        placement="bottom right"
+        isHovered={false}
+        isDisabled={false}
+        isPressed={false}
+      >
+        <FabIcon as={AddIcon} />
+        <FabLabel>evento</FabLabel>
+      </Fab>
+    </Box>
+  );
 }
